fea7c00006bc88d216b6236804b2ad5d9d0ac3e6
Geoffray Gaborit <gaborit.geoffray@icloud.com>
2022-07-03 02:00:55 +0200

    🔥

--- models/users.js
+++ models/users.js
@@ -7,6 +7,30 @@ const users = [
     id: 2,
     name: "Jane Doe",
   },
+  {
+    id: 3,
+    name: "Yohan Sczeny",
+  },
+  {
+    id: 4,
+    name: "Mark Temp",
+  },
+  {
+    id: 5,
+    name: "Patricia Belma",
+  },
+  {
+    id: 6,
+    name: "Ronald Giggs",
+  },
+  {
+    id: 7,
+    name: "Morgan Pete",
+  },
+  {
+    id: 8,
+    name: "Ashley Fring",
+  },
 ];
 
 module.exports = {
