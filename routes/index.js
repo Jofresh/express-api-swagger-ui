9c4e092459618d7028ebc17bf3c76c9a05c6c347
geo <46541094+Jofresh@users.noreply.github.com>
2022-07-11 14:31:08 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ const router = require("express").Router();
 const usersRouter = require("./users");
 const postsRouter = require("./posts");
 
-const verifyToken = require("../middleware/auth")
-
 router.get("/", (req, res) => res.redirect("/api-docs")); //res.sendFile(`${__dirname}/index.html`));
 
-router.use("/users", verifyToken, usersRouter);
-router.use("/posts", verifyToken, postsRouter);
+router.use("/users", usersRouter);
+router.use("/posts", postsRouter);
 
 module.exports = router;
