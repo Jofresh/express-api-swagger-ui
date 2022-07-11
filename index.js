be4b775c85167d431fb18c3a8435460fcc7c6a3f
geo <46541094+Jofresh@users.noreply.github.com>
2022-07-11 14:48:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use("/", routes);
 const swaggerSetup = require("./swagger-setup");
 swaggerSetup(app);
 
+// handling errors
+router.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "Something broke!" });
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App listening http://localhost:${port}`);
