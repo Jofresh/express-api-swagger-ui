e01a4dea9a7a507bf8a738face50215d7f0e7025
Geoffray Gaborit <46541094+Jofresh@users.noreply.github.com>
2022-08-19 11:24:31 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-// Dev only
 const router = require("express").Router();
 const usersRouter = require("./users");
 const postsRouter = require("./posts");
 
-router.get("/", (req, res) => res.redirect("/api-docs")); //res.sendFile(`${__dirname}/index.html`));
+router.get("/", (req, res) => res.redirect("/api-docs"));
 
 router.use("/users", usersRouter);
 router.use("/posts", postsRouter);
